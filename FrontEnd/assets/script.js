bc09cc3a39331778e6f266b3a3533f4da7f4bf57
MClaire D <mcdelap@gmail.com>
2025-12-17 11:40:16 +0100

    Ajout du script JavaScript pour la gestion dynamique de la galerie

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,31 @@
+//PROJETS
+const gallery = document.querySelector(".gallery");
+
+fetch("http://localhost:5678/api/works")
+.then(response => response.json())
+.then(works => {
+    works.forEach(work => {
+
+        const figure = document.createElement ("figure");
+        const img = document.createElement ("img");
+        const figcaption = document.createElement ("figcaption");
+        
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figcaption.textContent = work.title;
+        
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+
+        });
+
+});
+
+
+ 
+
+
+
+
+
